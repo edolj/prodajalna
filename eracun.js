77f0c93e23e1af1d3121da785404f03914f4efe3
Edo <edo.ljubijankic@hotmail.com>
2016-05-08 16:40:23 +0000

    veja1 z veja4

--- eracun.js
+++ eracun.js
@@ -195,12 +195,16 @@ var vrniRacune = function(callback) {
   );
 }
 
+var sporociloSpr = "";
+
 // Registracija novega uporabnika
 streznik.post('/prijava', function(zahteva, odgovor) {
   var form = new formidable.IncomingForm();
   
   form.parse(zahteva, function (napaka1, polja, datoteke) {
     var napaka2 = false;
+    sporociloSpr = "Stranka je bila uspešno registrirana.";
+    
     try {
       var stmt = pb.prepare("\
         INSERT INTO Customer \
@@ -208,14 +212,17 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Address, City, State, Country, PostalCode, \
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
-      //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      // TODO: done 
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State,
+                polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.finalize();
     } catch (err) {
       napaka2 = true;
+      sporociloSpr = "Prišlo je dp napake pri registraciji nove stranke. \
+                      Prosim preverite vnešene podatke in poskusite znova.";
     }
-  
-    odgovor.end();
+    
+    odgovor.redirect('/prijava');
   });
 })
 
@@ -223,9 +230,10 @@ streznik.post('/prijava', function(zahteva, odgovor) {
 streznik.get('/prijava', function(zahteva, odgovor) {
   vrniStranke(function(napaka1, stranke) {
       vrniRacune(function(napaka2, racuni) {
-        odgovor.render('prijava', {sporocilo: "", seznamStrank: stranke, seznamRacunov: racuni});  
+        odgovor.render('prijava', {sporocilo: sporociloSpr, seznamStrank: stranke, seznamRacunov: racuni});
+        sporociloSpr = "";
       }) 
-    });
+  });
 })
 
 // Prikaz nakupovalne košarice za stranko
